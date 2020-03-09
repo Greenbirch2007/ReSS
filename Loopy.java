2e28a728d8efd18616e3a933b6f63d8debdbe77b
Greenbirch2007 <291109028@qq.com>
2020-03-09 21:57:44 +0800

    到底类与对象两者之间有什么不同

---
+++ Loopy.java
@@ -0,0 +1,11 @@
+public class Loopy {
+
+    public static void main(String[] args) {
+        String[] wr = {"ass","safd"};
+
+        int onelength = wr.length;
+        int rand1 = (int)(Math.random()*onelength);
+
+        System.out.println(rand1);
+     }
+}
